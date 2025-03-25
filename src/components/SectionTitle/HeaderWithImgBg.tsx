32a37043f0c4684d46119228fb1579806ece77ce
Josh <jsbusiness305@gmail.com>
2025-03-25 17:21:52 -0400

    updated HeaderWithImgBg hover and text stlye

--- src/components/SectionTitle/HeaderWithImgBg.tsx
+++ src/components/SectionTitle/HeaderWithImgBg.tsx
@@ -10,23 +10,22 @@ export default function HeaderWithImgBg({
 }) {
   return (
     <>
-      <div className="relative w-full h-50">
+      <div className="relative w-full h-50 group">
         {/* Background with opacity */}
         <div
-          className="absolute inset-0" // Ensures the background spans the parent
+          className="absolute inset-0 opacity-30 group-hover:opacity-100 transition duration-300 ease-in" // Ensures the background spans the parent
           style={{
             backgroundImage: `url(${imgUrl})`,
             backgroundSize: "cover", // Ensures the image fits parent dimensions
             backgroundPosition: "center", // Centers the image within the parent
             backgroundRepeat: "no-repeat", // Prevents tiling of the image
-            opacity: "0.3",
             borderRadius: "10px",
             zIndex: 2, // Z-index for background
           }}
         ></div>
 
         {/* Header Title */}
-        <h1 className="absolute inset-0 flex items-center text-[3.5rem]  md:text-[7.5rem] p-10  verde z-[3]">
+        <h1 className="absolute inset-0 flex items-center text-[3.5rem]  md:text-[7.5rem] p-10  verde z-[3] group-hover:scale-105 transition duration-300 ease-in">
           {title}
         </h1>
       </div>
