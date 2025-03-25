37d1757da19a721bef044f6dae8c651f29abc00b
Josh <jsbusiness305@gmail.com>
2025-03-25 12:26:22 -0400

    updated modal style, padding and gap

--- src/components/Modal/ModalSignIn.tsx
+++ src/components/Modal/ModalSignIn.tsx
@@ -12,16 +12,16 @@ export default function ModalSignIn({ onClose }: { onClose: () => void }) {
 
       {/* Modal Content */}
       <div className="flex flex-col gap-5 rounded-xl bg-white p-10 z-[101]">
-        <div className="flex flex-row justify-end w-full">
-          <button onClick={onClose} className="hover:cursor-pointer p-5 bg-cyan-100 rounded-full">
+        <div className="flex flex-row items-center justify-between w-full">
+        <h1 className="verde text-7xl">Verde</h1>
+          <button onClick={onClose} className="hover:cursor-pointer p-5 bg-cyan-100 rounded-full hover:bg-cyan-200 transition duration-300 ease-in">
             <X size={30} />
           </button>
         </div>
-        <h1 className="verde text-7xl">Verde</h1>
         <span className="text-2xl">To favorite an item, please create an account</span>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-5">
           <Link href="/register">
-            <button className="hover:cursor-pointer verde bg-green-100 p-5 rounded-full hover:bg-green-200 transition duration-300 ease-in text-5xl">
+            <button className="hover:cursor-pointer verde hover:scale-105 bg-green-100 p-5 rounded-full hover:bg-green-200 transition duration-300 ease-in text-5xl">
               Continue to register
             </button>
           </Link>
