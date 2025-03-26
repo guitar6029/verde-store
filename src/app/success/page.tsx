bacf7b6d4de85963bf31cb7b501e53b53ecbf449
Josh <jsbusiness305@gmail.com>
2025-03-26 14:26:24 -0400

    removed unused page, updated success page style

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -24,11 +24,11 @@ export default function SuccessPage() {
   }, [clearCart, router]);
 
   return (
-    <div className="min-h-screen p-10">
+    <div className="min-h-screen p-10 flex flex-col">
       <HeaderWithImgBg title="Success" />
-      <div className="flex flex-col gap-2 justify-center items-center mx-auto">
+      <div className="flex flex-col gap-2 justify-center items-center mx-auto my-auto">
         <h1 className="text-4xl">Thank you for your order!</h1>
-        <Link href={"/"}>Back to Home</Link>
+        <Link href={"/"} className="text-2xl">Back to Home</Link>
       </div>
     </div>
   );
